8cffe99b2a2e2c6d77577a176143e647d5c9069b
Mike <72084528+mixa030590@users.noreply.github.com>
2021-10-21 16:23:02 +0300

    3 task

---
+++ task3-8kyu(Count of positives - sum of negatives).js
@@ -0,0 +1,16 @@
+//Given an array of integers.Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.If the input array is empty or null, return an empty array.
+function countPositivesSumNegatives(input) {
+  if (input.length < 1) {
+    return [];
+  }
+  var newArray = [0, 0];
+
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] > 0) {
+      newArray[0] += 1;
+    } else {
+      newArray[1] += input[i];
+    }
+  }
+  return newArray;
+}
