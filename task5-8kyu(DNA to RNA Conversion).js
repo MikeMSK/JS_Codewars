fc5e777fa022e8e6e73340e68376ff8076589bb7
Mike <72084528+mixa030590@users.noreply.github.com>
2021-10-21 22:49:29 +0300

    5 task

---
+++ task5-8kyu(DNA to RNA Conversion).js
@@ -0,0 +1,13 @@
+// Create a function which translates a given DNA string into RNA.
+
+function DNAtoRNA(dna) {
+  let str = "";
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] == "T") {
+      str += "U";
+    } else {
+      str += dna[i];
+    }
+  }
+  return str;
+}
